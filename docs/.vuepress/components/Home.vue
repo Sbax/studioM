e0de610d837aac03fd90cae4162bb7c5c94ccfe2
Matteo Bacci <docsbax@gmail.com>
2018-12-15 11:59:04 +0100

    Added icons

--- docs/.vuepress/components/Home.vue
+++ docs/.vuepress/components/Home.vue
@@ -52,17 +52,17 @@
                 <p>
                     <p>
                         <a href="tel:+390542643314">
-                            +39 0542 643314
+                            <img src="phone.svg" alt=""> +39 0542 643314
                         </a>
                     </p>
                     <p>
                         <a href="mailto:studiomparrucchieri@gmail.com">
-                            studiomparrucchieri@gmail.com
+                            <img src="mail.svg" alt=""> studiomparrucchieri@gmail.com
                         </a>
                     </p>
-                    <p class="address">
+                    <p>
                         <a href="https://www.google.it/maps/place/Studio+M+Di+Fabbri+Maurizio/@44.3771006,11.7263129,15z/data=!4m5!3m4!1s0x0:0x346d6bb38e4702a6!8m2!3d44.3771006!4d11.7263129">
-                            <img src="location-pin-white.svg" alt="">
+                            <img src="map-pin.svg" alt="">
                             Torre Lasie, via Lasie 10/L
                             40026 Imola
                         </a>
@@ -371,7 +371,7 @@ section.contacts {
         }
     }
 
-    .address {
+    article {
         img {
             width: 1em;
             height: 1em;
